175c638e8f7795e2dfca61a47d86dab1327d76da
coutinho98 <mateuscouto98@outlook.com>
2026-01-01 02:49:08 -0300

    feat: add sutdio normalization and custom stereo downmix matrix

--- main.py
+++ main.py
@@ -5,25 +5,32 @@ from pathlib import Path
 
 def convert_audio_to_stereo(input_path, output_file):
     try:
-        print(f"Iniciando conversão de: {input_path}...")
+        print(f"Processing: {input_path}...")
 
         input_stream = ffmpeg.input(input_path)
 
+        audio_filter = (
+            input_stream.audio
+            .filter('pan', 'stereo',
+                    FL='1.0*FL + 0.707*FC + 0.707*BL',
+                    FR='1.0*FR + 0.707*FC + 0.707*BR')
+            .filter('loudhorm'))
+
         out = ffmpeg.output(
-                           input_stream,
+                           input_stream.video,
+                           audio_filter,
                            output_file, 
                            vcodec='copy', 
                            acodec='aac', 
                            ac=2, 
-                           map_metadata=0)
+                           map_metadata=0
+                           )
 
         ffmpeg.run(out, overwrite_output=True, capture_stdout=True, capture_stderr=True)        
-        print(f"Sucesso! Arquivo salvo como: {output_file}")
+        print(f"Success: {output_file}")
 
     except ffmpeg.Error as e:
-        print(f"Ocorreu um erro no FFmpeg: {e.stderr.decode()}")
-    except Exception as e:
-        print(f"Ocorreu um erro: {str(e)}")
+        print(f"FFmpeg Error: {e.stderr.decode()}")
 
 def main():
     parser = argparse.ArgumentParser(description="Batch Downmix 5.1 Audio to 2.0 Stereo")
@@ -51,4 +58,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
