54f0240eb21db2702c4cfd424dcb6f2e8ab31853
coutinho98 <mateuscouto98@outlook.com>
2026-01-02 11:15:53 -0300

    feat: added log

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import ffmpeg
+import os
 import argparse
+import logging
 from pathlib import Path
+from datetime import datetime
+
+log_filename = f"conversion_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(log_filename, encoding='utf-8'),
+        logging.StreamHandler()
+    ]
+)
 
 def convert_audio_to_stereo(input_path, output_file):
     try:
-        print(f"processing: {input_path}...")
+        logging.info(f"Processing file: {os.path.basename(input_path)}")
 
         input_stream = ffmpeg.input(input_path)
 
@@ -26,10 +40,10 @@ def convert_audio_to_stereo(input_path, output_file):
                            )
 
         ffmpeg.run(out, overwrite_output=True, capture_stdout=True, capture_stderr=True)        
-        print(f"success: {output_file}")
+        logging.info(f"Successfully converted to stereo: {of.path.basename(output_file)}")
 
     except ffmpeg.Error as e:
-        print(f"ffmpeg error: {e.stderr.decode()}")
+       logging.error(f"FFmpeg error for file {os.path.basename(input_path)}: {e.stderr.decode()}")
 
 def main():
     parser = argparse.ArgumentParser(description="batch downmix 5.1 audio to 2.0 stereo")
@@ -42,14 +56,17 @@ def main():
 
     if not output_dir.exists():
         output_dir.mkdir()
+        logging.info(f"Created output directory: {output_dir}")
 
     if input_path.is_file():
         files_to_process = [input_path]
     elif input_path.is_dir():
         files_to_process = list(input_path.glob("*.mkv")) + list(input_path.glob("*.mp4"))
     else:
-        print("Error: Input path not found.")
+        logging.error("Input path is neither a file nor a directory.")
         return
+    
+    logging.info(f"Found {len(files_to_process)} files to process.")    
 
     for file in files_to_process:
         destination = output_dir / f"{file.stem}_stereo{file.suffix}"
