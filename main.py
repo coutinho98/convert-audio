2c9461352e86105854972269e80f11e455284b13
coutinho98 <mateuscouto98@outlook.com>
2026-01-02 10:57:31 -0300

    command to start and gitignore

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import ffmpeg
-import os
 import argparse
 from pathlib import Path
 
 def convert_audio_to_stereo(input_path, output_file):
     try:
-        print(f"Processing: {input_path}...")
+        print(f"processing: {input_path}...")
 
         input_stream = ffmpeg.input(input_path)
 
         audio_filter = (
             input_stream.audio
             .filter('pan', 'stereo',
-                    FL='1.0*FL + 0.707*FC + 0.707*BL',
+                    FL='1.0*FL + 0.707*FC + 0.707*BL', # needs testing 
                     FR='1.0*FR + 0.707*FC + 0.707*BR')
             .filter('loudhorm'))
-
+ 
         out = ffmpeg.output(
                            input_stream.video,
                            audio_filter,
@@ -27,15 +26,15 @@ def convert_audio_to_stereo(input_path, output_file):
                            )
 
         ffmpeg.run(out, overwrite_output=True, capture_stdout=True, capture_stderr=True)        
-        print(f"Success: {output_file}")
+        print(f"success: {output_file}")
 
     except ffmpeg.Error as e:
-        print(f"FFmpeg Error: {e.stderr.decode()}")
+        print(f"ffmpeg error: {e.stderr.decode()}")
 
 def main():
-    parser = argparse.ArgumentParser(description="Batch Downmix 5.1 Audio to 2.0 Stereo")
-    parser.add_argument("input", help="Input file or directory")
-    parser.add_argument("-o", "--output", help="Output directory (optional)", default="output_stereo")
+    parser = argparse.ArgumentParser(description="batch downmix 5.1 audio to 2.0 stereo")
+    parser.add_argument("input", help="input file or directory")
+    parser.add_argument("-o", "--output", help="output directory (optional)", default="output_stereo")
 
     args = parser.parse_args()
     input_path = Path(args.input)
@@ -58,3 +57,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# python main.py film_name
+# python main.py ./folder/
